be53eff047607eca541a6ca2fa1ebdab7ff25b00
Toluwani 🔗 <israelmakanjuola7@gmail.com>
2024-10-11 01:57:06 +0100

    second commit

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import './Coin.css'
 import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext'
 
 const Coin = () => {
 
   const {coinId} = useParams();
+  const [coinData, setCoinData] = useState();
+  const {currency} = useContext(CoinContext);
 
-  return (
-    <div>
-      <h2>Coin : {coinId}</h2>
-    </div>
-  )
+  const fetchCoinData = async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-cBqRHmhLj8k6u6Dc8YisnQby'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(response => response.json())
+      .then(response => setCoinData(response))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+    fetchCoinData();
+  },[currency])
+
+
+  
+  if (coinData){
+    return (
+      <div className='coin'>
+        <div className='coin-name'>
+          <img src={coinData.image.large} alt="" />
+          <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        </div>
+      </div>
+    )
+  }else{
+    return (
+      <div className='spinner'>
+        <div className='spin'>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Coin
